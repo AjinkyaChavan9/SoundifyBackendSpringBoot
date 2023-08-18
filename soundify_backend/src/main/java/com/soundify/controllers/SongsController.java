48856459e4e42ffcc274e061e0b013f0fa55a7d4
shubham19981808 <shubhamminister19@gmail.com>
2023-08-18 11:43:30 -0700

    added SongsController

---
+++ soundify_backend/src/main/java/com/soundify/controllers/SongsController.java
@@ -0,0 +1,10 @@
+package com.soundify.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // =@Controller at cls level + @ResponseBody added over ret
+@RequestMapping("/api/songs")
+public class SongsController {
+
+}
