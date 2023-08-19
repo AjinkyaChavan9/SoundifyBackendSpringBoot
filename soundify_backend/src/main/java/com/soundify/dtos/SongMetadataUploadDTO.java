0c49a3101e0a0bfae9c68e9e2be7f4b446fd0fb9
Ajinkya Chavan <AjinkyaChavan9@users.noreply.github.com>
2023-08-19 20:22:50 +0530

    ServerSideSongUploadDownload feature completed

--- soundify_backend/src/main/java/com/soundify/dtos/SongMetadataUploadDTO.java
+++ soundify_backend/src/main/java/com/soundify/dtos/SongMetadataUploadDTO.java
@@ -6,11 +6,13 @@ import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class SongMetadataUploadDTO {
 	
 	private String songName;
