65d40dbebc93c03fe230ae2ea5fc2172743e4041
Ajinkya Chavan <AjinkyaChavan9@users.noreply.github.com>
2023-08-20 18:59:53 +0530

    Artist add/remove song feature completed

--- soundify_backend/src/main/java/com/soundify/controllers/PlaylistController.java
+++ soundify_backend/src/main/java/com/soundify/controllers/PlaylistController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soundify.dtos.ApiResponse;
 import com.soundify.services.PlaylistService;
 
 @RestController
@@ -27,18 +28,18 @@ public class PlaylistController {
 	 @PostMapping("/{playlistId}/song/{songId}")
 	 public ResponseEntity<?> addSongToPlaylist(@PathVariable Long playlistId, @PathVariable Long songId) {
 	    playlistService.addSongToPlaylist(playlistId, songId);
-	    return ResponseEntity.status(HttpStatus.CREATED).body("Song Uploaded in Playlist");
+	    return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("Song Uploaded in Playlist"));
 	 }
 	 @DeleteMapping("/{playlistId}/song/{songId}")
 	 public ResponseEntity<?> removeSongFromPlaylist(@PathVariable Long playlistId, @PathVariable Long songId) {
 	        playlistService.removeSongFromPlaylist(playlistId, songId);
-	        return ResponseEntity.status(HttpStatus.CREATED).body("Song Removed from Playlist");
+	        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("Song Removed from Playlist"));
 	 }
 	 
 	 @PutMapping("/{playlistId}/")
 	 public ResponseEntity<?> updatePlaylistName(@PathVariable Long playlistId, @RequestBody String playlistName) {
 	        playlistService.updatePlaylistName(playlistId, playlistName);
-	        return ResponseEntity.ok("Playlist Name updated successfully!");
+	        return ResponseEntity.ok(new ApiResponse("Playlist Name updated successfully!"));
 	 }
 
 }
--- soundify_backend/src/main/java/com/soundify/services/ArtistServiceImpl.java
+++ soundify_backend/src/main/java/com/soundify/services/ArtistServiceImpl.java
@@ -1,6 +1,5 @@
 package com.soundify.services;
 
-
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
@@ -11,11 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.soundify.custom_exceptions.ResourceNotFoundException;
 import com.soundify.daos.ArtistDao;
+import com.soundify.daos.SongDao;
 import com.soundify.dtos.artists.ArtistSigninRequestDTO;
 import com.soundify.dtos.artists.ArtistSigninResponseDTO;
 import com.soundify.dtos.artists.ArtistSignupRequestDTO;
 import com.soundify.dtos.artists.ArtistSignupResponseDTO;
 import com.soundify.entities.Artist;
+import com.soundify.entities.Song;
 
 @Service
 @Transactional
@@ -24,55 +25,72 @@ public class ArtistServiceImpl implements ArtistService {
 	private ArtistDao artDao;
 	
 	@Autowired
+	private SongDao songDao;
+
+	@Autowired
 	private ModelMapper mapper;
 
 	@Override
 	public ArtistSignupResponseDTO addArtDetails(ArtistSignupRequestDTO artDTO) {
 		System.out.println("request  " + artDTO);
-		//Since we need entity for persistence , map , dto ----> Entity --> then invoke save
+		// Since we need entity for persistence , map , dto ----> Entity --> then invoke
+		// save
 		Artist persistentArt = artDao.save(mapper.map(artDTO, Artist.class));
-		//If we send directly entity to REST clnt , it will contain , 
-		//un necessary fields(eg : password , dept , address, projects...) , 
-		//so as a standard suggestion , map entity --> dto	
+		// If we send directly entity to REST clnt , it will contain ,
+		// un necessary fields(eg : password , dept , address, projects...) ,
+		// so as a standard suggestion , map entity --> dto
 		return mapper.map(persistentArt, ArtistSignupResponseDTO.class);
 	}
-	
 
 	@Override
 	public ArtistSigninResponseDTO signInArtist(ArtistSigninRequestDTO artSignInRequestDTO) {
 		System.out.println("request  " + artSignInRequestDTO);
-		//Since we need entity for persistence , map , dto ----> Entity --> then invoke save
-		Artist persistentArt = artDao.findByEmailAndPassword(artSignInRequestDTO.getEmail(),artSignInRequestDTO.getPassword()).orElseThrow(() -> new ResourceNotFoundException("artist not found "));
-		//If we send directly entity to REST clnt , it will contain , 
-		//un necessary fields(eg : password , dept , address, projects...) , 
-		//so as a standard suggestion , map entity --> dto		
+		// Since we need entity for persistence , map , dto ----> Entity --> then invoke
+		// save
+		Artist persistentArt = artDao
+				.findByEmailAndPassword(artSignInRequestDTO.getEmail(), artSignInRequestDTO.getPassword())
+				.orElseThrow(() -> new ResourceNotFoundException("artist not found "));
+		// If we send directly entity to REST clnt , it will contain ,
+		// un necessary fields(eg : password , dept , address, projects...) ,
+		// so as a standard suggestion , map entity --> dto
 		return mapper.map(persistentArt, ArtistSigninResponseDTO.class);
-		
-	
-	}
 
+	}
 
 	@Override
 	public ArtistSigninResponseDTO updateArtist(ArtistSignupResponseDTO artist, Long Id) {
-		 Artist existingArtist = artDao.findById(Id)
-		            .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+		Artist existingArtist = artDao.findById(Id).orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-		    // Update user properties based on the updatedUser DTO
-		 existingArtist.setFirstName(artist.getFirstName());
-		 existingArtist.setLastName(artist.getLastName());
-		 existingArtist.setEmail(artist.getEmail());
-		 existingArtist.setDateOfBirth(artist.getDateOfBirth());
-		   // Update other properties as needed
+		// Update user properties based on the updatedUser DTO
+		existingArtist.setFirstName(artist.getFirstName());
+		existingArtist.setLastName(artist.getLastName());
+		existingArtist.setEmail(artist.getEmail());
+		existingArtist.setDateOfBirth(artist.getDateOfBirth());
+		// Update other properties as needed
 
-		    // Save the updated user
-		 existingArtist = artDao.save(existingArtist);
+		// Save the updated user
+		existingArtist = artDao.save(existingArtist);
 
-		    return mapper.map(existingArtist, ArtistSigninResponseDTO.class);
-	
+		return mapper.map(existingArtist, ArtistSigninResponseDTO.class);
+
 	}
-	
-	//MODIFIED to add DTO pattern
-	
 
-		
+	// MODIFIED to add DTO pattern
+	
+	 public void addSongToArtist(Long artistId, Long songId) {
+	        Artist artist = artDao.findById(artistId).orElseThrow(() -> new ResourceNotFoundException("Artist not found"));
+	        Song song = songDao.findById(songId).orElseThrow(() -> new ResourceNotFoundException("Song not found"));
+
+	        artist.addSong(song);
+	        artDao.save(artist);
+	    }
+
+	    public void removeSongFromArtist(Long artistId, Long songId) {
+	        Artist artist = artDao.findById(artistId).orElseThrow(() -> new ResourceNotFoundException("Artist not found"));
+	        Song song = songDao.findById(songId).orElseThrow(() -> new ResourceNotFoundException("Song not found"));
+
+	        artist.removeSong(song);
+	        artDao.save(artist);
+	    }
+
 }
