1cc61985ca7556f1d3011a9f6c871919fc8eda13
Ajinkya Chavan <AjinkyaChavan9@users.noreply.github.com>
2023-08-28 01:16:28 +0530

    ManyToMany Deletion custom queries added

--- soundify_backend/src/main/java/com/soundify/daos/ArtistDao.java
+++ soundify_backend/src/main/java/com/soundify/daos/ArtistDao.java
@@ -3,10 +3,17 @@ package com.soundify.daos;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.soundify.entities.Artist;
 
 public interface ArtistDao extends JpaRepository<Artist, Long> {
 	// add a finder method for artist's signin
 	Optional<Artist> findByEmailAndPassword(String ar, String pass);
+
+	@Modifying
+	@Query(value = "DELETE FROM artist_follower WHERE artist_id = :artistId", nativeQuery = true)
+	void removeFollowersFromArtist(Long artistId);
 
 }
